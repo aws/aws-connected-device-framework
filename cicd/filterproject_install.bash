73866b9bd540a0d9fedc0aa9cecc0127b8658e03
Hart <deanhart@f01898979652.ant.amazon.com>
2019-03-02 22:15:56 -0700

    legwork to determine which packages to version and deploy

--- cicd/filterproject_install.bash
+++ cicd/filterproject_install.bash
@@ -14,20 +14,8 @@ git config --global user.email "$CDF_CODECOMMIT_EMAIL"
 git config --global user.name "$CDF_CODECOMMIT_USERNAME"
 
 echo Adding workaround for CodeCommit not cloning source as git
-cd $CODEBUILD_SRC_DIR
-
-if [ "$BRANCH" = "master" ]; then
-    # no need to fetch all the history
-    cd .. && git clone --depth=1 "https://$ESCAPED_CREDENTIALS@git-codecommit.$AWS_REGION.amazonaws.com/v1/repos/$REPO_NAME" temp_src
-else
-    cd .. && git clone "https://$ESCAPED_CREDENTIALS@git-codecommit.$AWS_REGION.amazonaws.com/v1/repos/$REPO_NAME" temp_src
-    cd temp_src && git checkout -t origin/$BRANCH && cd ..
-fi
+cd $CODEBUILD_SRC_DIR/..
+git clone "https://$ESCAPED_CREDENTIALS@git-codecommit.$AWS_REGION.amazonaws.com/v1/repos/$REPO_NAME" temp_src
+cd temp_src && git checkout $CODEBUILD_RESOLVED_SOURCE_VERSION && cd ..
 rm -rf "$CODEBUILD_SRC_DIR/*"
 cp -a temp_src/. "$CODEBUILD_SRC_DIR/" && rm -rf temp_src
-
-
-
-
-
-
--- cicd/filterproject_prebuild.bash
+++ cicd/filterproject_prebuild.bash
@@ -8,15 +8,56 @@ printenv | sort
 
 cd $CODEBUILD_SRC_DIR
 
-#  retrieve the latest git commit message for the source.
+#  retrieve the latest git message for this specific commit
 git_commit_message="$(git log --format=%B -n 1)"
-
 echo git_commit_message: $git_commit_message
 
-if [[ $git_commit_message == CICD* ]]; then
+# don't proceed if the commit was made by the CICD pipeline
+if [[ $git_commit_message == CICD:* ]]; then
     echo A CICD commit, therefore ignoring
     exit 1
-else
-    echo Proceeding with build
-    exit 0
 fi
+
+
+echo "Determining which packages have changed"
+changed_paths=()
+# check to see if a previous release has been made (the CICD_LATEST_RELEASE tag is set when a cicd pipeline release is successful)
+if git rev-parse CICD_LATEST_RELEASE >/dev/null 2>&1; then
+
+    # determine which file paths have changed between the last known release and the specific commit that triggered this pipeline
+    changed_files=$(git log --name-only --pretty=oneline --full-index CICD_LATEST_RELEASE..$CODEBUILD_RESOLVED_SOURCE_VERSION | grep -vE '^[0-9a-f]{40} ' | sort | uniq)
+    echo changed_files: $changed_files
+
+    # loop through each of the changed files to filter out paths that should not trigger a release
+    for file in $changed_files; do
+
+        case "$file" in
+            "."* ) 
+                # ignore any hidden/system files
+                continue;;
+            "cicd/"* )
+                # ignore any changes to the cicd/ directory
+                continue;;
+            "packages/libraries/"* )
+                # libraries are located 4 levels deep
+                changed_path=$(cut -d'/' -f 1-4 <<< "$file")
+                changed_paths+=(changed_path);;
+            "packages/services/"* )
+                # services are located 3 levels deep
+                changed_path=$(cut -d'/' -f 1-3 <<< "$file")
+                changed_paths+=(changed_path);;
+            * )
+                # everything else, which should be root,trigger a full build
+                changed_paths+=('ALL');;
+        esac
+
+    done
+else
+    echo "Initial deployment, therefore mark all projects as needing to be processed"
+    changed_paths+=('ALL');;
+fi
+unique_changed_paths=$(echo $changed_paths | sort | uniq)
+
+echo "Store the unique changed paths"
+echo "$unique_changed_paths" > ".unique_changed_paths"
+cat .unique_changed_paths
