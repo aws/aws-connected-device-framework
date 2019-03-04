affee1395beebc6358d49ee4987dda3662423fed
Dean Hart <deanhart@amazon.com>
2019-03-03 19:58:58 -0700

    build(cicd): semantic-release issues
    @semantic-release/changelog was not installing in the root node_modules

--- cicd/buildproject_prebuild.sh
+++ cicd/buildproject_prebuild.sh
@@ -5,7 +5,7 @@ set -e
 echo buildproject_prebuild started on `date`
 
 echo Installing...
-pnpm m i
+pnpm i
 
 echo Checking to see if we have changes that need committing...
 set +e
@@ -14,7 +14,7 @@ set -e
 if [ $differences -eq 1 ]; then
     echo Yes we have changes, lets commit them...
     # package-lock.json probably changed.  needs committing for the npm versioning to function
-    git commit -am '[CICD]: buildproject_prebuild'
+    git commit -am 'ci(@cdf) buildproject_prebuild [skip ci]'
 fi
 
 
