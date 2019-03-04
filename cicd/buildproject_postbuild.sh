c13f07a15f8d856ee08286e289b4b00851e5450e
Dean Hart <deanhart@amazon.com>
2019-03-04 06:56:05 -0700

    build(cicd): perform semantic-release at the leaf packages
    running at the root package was not detecting changes

--- cicd/buildproject_postbuild.sh
+++ cicd/buildproject_postbuild.sh
@@ -6,4 +6,4 @@ echo buildproject_postbuild started on `date`
 
 echo versioning...
 export GIT_CREDENTIALS="$CDF_CODECOMMIT_USERNAME:$CDF_CODECOMMIT_PASSWORD"
-pnpm run semantic-release -- --debug
+pnpm m run semantic-release -- --debug
