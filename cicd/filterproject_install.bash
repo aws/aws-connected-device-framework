e1e1559782fa0efc8d142e0e663db0e74755b234
Dean Hart <deanhart@amazon.com>
2019-03-05 15:26:33 -0700

    ci(cicd): init git config as local
    so gets passed to downstream CodeBuild projects

--- cicd/filterproject_install.bash
+++ cicd/filterproject_install.bash
@@ -9,13 +9,14 @@ env | sort
 echo Adding workaround for unsupported characters in the auto-generated codecommit passwords
 export ESCAPED_CREDENTIALS=$(echo "$CDF_CODECOMMIT_USERNAME:$CDF_CODECOMMIT_PASSWORD" | sed -r 's:/:%2F:g' )
 
-echo configuring git
-git config user.email "$CDF_CODECOMMIT_EMAIL"
-git config user.name "$CDF_CODECOMMIT_USERNAME"
-
 echo Adding workaround for CodeCommit not cloning source as git
 cd $CODEBUILD_SRC_DIR/..
 git clone "https://$ESCAPED_CREDENTIALS@git-codecommit.$AWS_REGION.amazonaws.com/v1/repos/$REPO_NAME" temp_src
 cd temp_src && git checkout $CODEBUILD_RESOLVED_SOURCE_VERSION && cd ..
 rm -rf "$CODEBUILD_SRC_DIR/*"
 cp -a temp_src/. "$CODEBUILD_SRC_DIR/" && rm -rf temp_src
+
+echo configuring git
+cd $CODEBUILD_SRC_DIR
+git config user.email "$CDF_CODECOMMIT_EMAIL"
+git config user.name "$CDF_CODECOMMIT_USERNAME"
