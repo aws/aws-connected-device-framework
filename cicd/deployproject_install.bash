b811d5996793092b1662ee029b30e99b039598ce
Dean Hart <deanhart@amazon.com>
2019-03-05 12:27:33 -0700

    ci(cicd): troubleshooting

--- cicd/deployproject_install.bash
+++ cicd/deployproject_install.bash
@@ -4,6 +4,8 @@ set -e
 
 echo deployproject_install started on `date`
 
+env | sort
+
 echo installing jq...
 curl -s -qL -o /usr/bin/jq https://stedolan.github.io/jq/download/linux64/jq
 chmod +x /usr/bin/jq
