4fc5bc343802e97cebb85b5782c07d8c4108a2fd
Dean Hart <deanhart@amazon.com>
2019-03-04 17:19:14 -0700

    build(@cdf/assetlibrary): added mime dependency
    a transitive dependency of assetlibrary (via send) but is not being bundled for some reason

--- cicd/deployproject_postbuild.sh
+++ cicd/deployproject_postbuild.sh
@@ -7,7 +7,7 @@ echo deployproject_postbuild started on `date`
 if [ "$CODEBUILD_BUILD_SUCCEEDING" -eq 1 ]; then
 
     echo tagging release...
-    tagName="@cdf-RELEASE-$(date +%s)"
+    tagName="RELEASE-$(date +%s)"
     git tag $tagName
     git push origin $tagName
 
