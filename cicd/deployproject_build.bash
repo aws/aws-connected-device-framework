39476baf284b8c6783ed4a2dddcc42043ee779f9
Dean Hart <deanhart@amazon.com>
2019-03-05 14:04:34 -0700

    ci(cicd): use same config file for both stage and live deploys

--- cicd/deployproject_build.bash
+++ cicd/deployproject_build.bash
@@ -8,6 +8,8 @@ echo deployproject_build started on `date`
 CONFIG_ENVIRONMENT=${ENVIRONMENT%-staging}
 
 export ASSETLIBRARY_CONFIG_LOCATION="$CODEBUILD_SRC_DIR_source_infrastructure/assetlibrary/${CONFIG_ENVIRONMENT}-config.json"
+echo deploying using configuration from $ASSETLIBRARY_CONFIG_LOCATION
+
 
 bash -c packages/services/assetlibrary/infrastructure/package-cfn.bash
 bash -c packages/services/assetlibrary/infrastructure/deploy-cfn.bash
