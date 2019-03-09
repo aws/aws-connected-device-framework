6f60c926f3088c7e198ee39807d4e996cc2ab8ed
Dean Hart <deanhart@amazon.com>
2019-03-09 10:27:59 -0700

    build(cicd): reduce concurrent pnpm workers
    experiencing intermittent errors when semantic-release is attempting to publish to git, therefore
    trying with cincurrenyc reduced from default of 4 to 1

--- cicd/buildproject_postbuild.bash
+++ cicd/buildproject_postbuild.bash
@@ -9,7 +9,7 @@ if [ "$CODEBUILD_BUILD_SUCCEEDING" -eq 1 ]; then
     echo versioning...
     echo Adding workaround for unsupported characters in the auto-generated codecommit passwords
     export GIT_CREDENTIALS=$(echo "$CDF_CODECOMMIT_USERNAME:$CDF_CODECOMMIT_PASSWORD" | sed -r 's:/:%2F:g' )
-    pnpm m run semantic-release -- --debug
+    pnpm m run semantic-release -- --workspace-concurrency 1 --debug
 
     echo Bundling...
     pnpm m run bundle
