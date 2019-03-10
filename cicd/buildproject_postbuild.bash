de9f8c8bc2b01002fb246c8fd020b7d314ecd062
Dean Hart <deanhart@amazon.com>
2019-03-09 19:27:59 -0700

    build(cicd): don't clean after building for unit testing

--- cicd/buildproject_postbuild.bash
+++ cicd/buildproject_postbuild.bash
@@ -8,7 +8,9 @@ if [ "$CODEBUILD_BUILD_SUCCEEDING" -eq 1 ]; then
 
     echo versioning...
     echo Adding workaround for unsupported characters in the auto-generated codecommit passwords
+    ### pnpm requires GIT_CREDENTIALS configuring for when it pushes version changes to codecommit 
     export GIT_CREDENTIALS=$(echo "$CDF_CODECOMMIT_USERNAME:$CDF_CODECOMMIT_PASSWORD" | sed -r 's:/:%2F:g' )
+    ### using the default concurrency of 4 causes CodeCommit to throw 429 errors, therefore perform the semantic release using just 1 thread
     pnpm m run semantic-release --workspace-concurrency 1
 
     echo Bundling...
