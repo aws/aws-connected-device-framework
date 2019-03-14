a51913feefdc84a97f1efffdb12c4f20960d4456
Dean Hart <deanhart@amazon.com>
2019-03-14 14:19:13 -0600

    ci(@cdf): publish compiled artifacts

--- cicd/deployproject_postbuild.bash
+++ cicd/deployproject_postbuild.bash
@@ -4,8 +4,44 @@ set -e
 
 echo deployproject_postbuild started on `date`
 
+
+function publish_artifacts() {
+    bundleName="$1.zip"
+    basedir=$(dirname $(pwd))
+
+    rm -rf $basedir/bundled
+    releasedir=$basedir/bundled
+    mkdir $releasedir
+
+    ### copy the main infrastructure scripts/templates
+    cp -R $basedir/infrastructure $releasedir/
+
+    ### copy each of the pre-compiled packages along with its related infrastructure scripts/templates
+    for package in $PACKAGES_TO_PUBLISH; do
+        mkdir -p $releasedir/packages/services/$package/build
+        cp $basedir/packages/services/$package/build/build.zip $releasedir/packages/services/$package/build/
+        cp -R $basedir/packages/services/$package/infrastructure $releasedir/packages/services/$package/
+    done
+
+    ### copy the integration tests
+    cp -R $basedir/packages/integration-tests $releasedir/packages/
+
+    ### copy the documentation
+    cp -R $basedir/documentation $releasedir/
+
+    ### zip and save to s3
+    zip -r "$bundleName" $releasedir/*
+    aws s3 cp "$bundleName" "$PUBLISH_LOCATION/$bundleName"
+
+}
+
+
+
 if [ "$CODEBUILD_BUILD_SUCCEEDING" -eq 1 ]; then
 
+    ### If the deploy was successful ....
+
+    ### First lets tag this release so we can always identify specific version of source code...
     echo tagging release...
     if [[ $ENVIRONMENT == *"-staging" ]]; then
         DEPLOY_ENV='STAGING'
@@ -20,5 +56,11 @@ if [ "$CODEBUILD_BUILD_SUCCEEDING" -eq 1 ]; then
         git tag -f $tagName
         git push -f origin $tagName
     done
+
+    ### Next, if this was a live deploy, publish the artifacts as an installable package
+    if [ "$DEPLOY_ENV"="LIVE" ]; then
+        echo publishing artifacts...
+        publish_artifacts tagNames[0]
+    fi
 
 fi
