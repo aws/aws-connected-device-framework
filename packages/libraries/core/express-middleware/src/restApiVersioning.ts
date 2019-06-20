a8659ad11e9fec1d18b91f51b462089306b14f63
Dean Hart <deanhart@amazon.com>
2019-06-20 08:10:22 -0600

    fix(versioning): fixed supported versioning of rest apis
    previously versioning was only enforced for post body requests, now includes all.  Supported
    versions now specified in config.  Sets foundation for introducing breaking changes that may require
    a new version, plus supporting alternate mime types such as hal+json (will be required for
    discovery).

---
+++ packages/libraries/core/express-middleware/src/restApiVersioning.ts
@@ -0,0 +1,36 @@
+
+export type SupportedVersionConfig = {[subType:string]:string[]}[];
+
+const MIME_TYPE_PREFIX = 'application/vnd.aws-cdf-v';
+
+export function asArray(supportedVersions:SupportedVersionConfig ): string[] {
+  const mimeTypes:string[]= [];
+  supportedVersions.forEach(sv=> {
+    Object.keys(sv).forEach(subType=> {
+      sv[subType].forEach(version=> {
+        const mimeType = `${MIME_TYPE_PREFIX}${version}${CdfVersionType[subType]}`;
+        mimeTypes.push(mimeType);
+      });
+    });
+  });
+  console.log(`Supported versions: ${JSON.stringify(mimeTypes)}`);
+  return mimeTypes;
+}
+
+export function supportsAtLeastVersion(accept:string, subType:CdfVersionType, version?:number, ): boolean {
+  if(subType===CdfVersionType.json && accept.endsWith(CdfVersionType.hal)) {
+    // explicit check, as we cannot do a simple endswith check due to duplicated types ending with +json
+    return false;
+  }
+  if (!accept.endsWith(subType)) {
+    return false;
+  }
+  const acceptsVersion = Number(accept.replace(MIME_TYPE_PREFIX, '').replace(subType, ''));
+  return acceptsVersion >= version;
+
+}
+
+export enum CdfVersionType {
+  json = '+json',
+  hal = 'hal+json'
+}
