27a5f900d433069d8a7c22ee93c540309d38595c
Dean Hart <deanhart@amazon.com>
2019-09-12 11:48:03 -0600

    fix(commands): tslint issues

--- packages/services/commands/src/utils/errors.ts
+++ packages/services/commands/src/utils/errors.ts
@@ -12,19 +12,15 @@ export function handleError(e:Error, res:Response): void {
     if (e.name === 'ArgumentError' || e.message.startsWith('MISSING_REQUIRED')
      || e.message === 'FAILED_VALIDATION' || e.message === 'UNSUPPORTED_TRANSITION' ) {
         res.status(400).json({error: e.message}).end();
-    
     } else if (e.message === 'NOT_FOUND') {
         res.status(404).json({error: 'Item not found'}).end();
     } else if (e.name==='ResourceNotFoundException') {
         res.status(404).json({error: e.message}).end();
-
     } else if (e.name === 'ConditionalCheckFailedException' ||
             e.message.indexOf('with id already exists')>=0 ) {
         res.status(409).json({error: 'Item already exists'}).end();
-
     } else if (e.message === 'TEMPLATE_IN_USE' ) {
         res.status(409).json({error: 'Template in use'}).end();
-
     } else {
         res.status(500).json({error: e.message}).end();
     }
