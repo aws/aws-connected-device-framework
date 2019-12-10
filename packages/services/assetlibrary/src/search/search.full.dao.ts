77deda85eec28974a3d60f68c9f80e7e8e771dfd
Khokhar, Hassan <hkhokhar@amazon.com>
2019-12-10 17:18:15 +0000

    Squashed commit of the following
    commit 0d778e8d 
    Author: Khokhar, Hassan <hkhokhar@amazon.com> 
    Date: Mon Dec 09 2019 12:24:02 GMT-0700 (Mountain Standard Time) 
    
        fix: lint
    
    
    commit e9d5bf15 
    Author: Khokhar, Hassan <hkhokhar@amazon.com> 
    Date: Mon Dec 09 2019 11:18:28 GMT-0700 (Mountain Standard Time) 
    
        fix: handle connection lifecycle from connect to close properly

--- packages/services/assetlibrary/src/search/search.full.dao.ts
+++ packages/services/assetlibrary/src/search/search.full.dao.ts
@@ -10,7 +10,7 @@ import {Node} from '../data/node';
 import {TYPES} from '../di/types';
 import { SearchRequestModel, SearchRequestFilterDirection, SearchRequestFilter, SearchRequestFacet, FacetResults } from './search.models';
 import {NodeAssembler} from '../data/assembler';
-import { BaseDaoFull } from '../data/base.full.dao';
+import {BaseDaoFull, NeptuneConnection} from '../data/base.full.dao';
 
 const __ = process.statics;
 
@@ -25,101 +25,97 @@ export class SearchDaoFull extends BaseDaoFull {
         super(neptuneUrl, graphSourceFactory);
     }
 
-    private buildSearchTraverser(request: SearchRequestModel, authorizedPaths:string[], offset?:number, count?:number) : process.GraphTraversal {
+    private buildSearchTraverser(conn: NeptuneConnection,request: SearchRequestModel, authorizedPaths:string[], offset?:number, count?:number) : process.GraphTraversal {
 
         logger.debug(`search.full.dao buildSearchTraverser: in: request: ${JSON.stringify(request)}, authorizedPaths:${authorizedPaths}, offset:${offset}, count:${count}`);
 
         const filters: process.GraphTraversal[]= [];
 
         // if a path is provided, that becomes the starting point
-        const conn = super.getConnection();
-        try {
-            let traverser: process.GraphTraversal;
-            if (request.ancestorPath!==undefined) {
-                const ancestorId = `group___${request.ancestorPath}`;
-                traverser = conn.traversal.V(ancestorId).
-                    repeat(__.in_()).emit().as('a');
-            } else {
-                traverser = conn.traversal.V().as('a');
-            }
-
-            // if authz is enabled, only return results that the user is authorized to view
-            if (authorizedPaths!==undefined && authorizedPaths.length>0) {
-                const authorizedPathIds = authorizedPaths.map(p=> `group___${p}`);
-                filters.push(__.as('a').until(__.hasId(...authorizedPathIds)).repeat(__.out()));
-            }
-
-            // construct all the filters that we will eventually pass to match()
-            if (request.types!==undefined) {
-                request.types.forEach(t=> filters.push(
-                    __.as('a').hasLabel(t)) );
-            }
-
-            if (request.eq!==undefined) {
-                request.eq.forEach(f=> {
-                    const filter = this.buildSearchFilterBase(f);
-                    filters.push(filter.has(f.field, f.value));
-                });
-            }
-            if (request.neq!==undefined) {
-                request.neq.forEach(f=> {
-                    const filter = this.buildSearchFilterBase(f);
-                    filters.push(filter.not(__.has(f.field, f.value)));
-                });
-            }
-            if (request.lt!==undefined) {
-                request.lt.forEach(f=> {
-                    const filter = this.buildSearchFilterBase(f);
-                    filters.push(filter.values(f.field).is(process.P.lt(Number(f.value))));
-                });
-            }
-            if (request.lte!==undefined) {
-                request.lte.forEach(f=> {
-                    const filter = this.buildSearchFilterBase(f);
-                    filters.push(filter.values(f.field).is(process.P.lte(Number(f.value))));
-                });
-            }
-            if (request.gt!==undefined) {
-                request.gt.forEach(f=> {
-                    const filter = this.buildSearchFilterBase(f);
-                    filters.push(filter.values(f.field).is(process.P.gt(Number(f.value))));
-                });
-            }
-            if (request.gte!==undefined) {
-                request.gte.forEach(f=> {
-                    const filter = this.buildSearchFilterBase(f);
-                    filters.push(filter.values(f.field).is(process.P.gte(Number(f.value))));
-                });
-            }
-            if (request.startsWith!==undefined) {
-                request.startsWith.forEach(f=> {
-                    const startValue = <string> f.value;
-                    const nextCharCode = String.fromCharCode( startValue.charCodeAt(startValue.length-1) + 1);
-                    const endValue = this.setCharAt(startValue, startValue.length-1, nextCharCode);
-
-                    const filter = this.buildSearchFilterBase(f);
-                    filters.push(filter.has(f.field, process.P.between(startValue, endValue)));
-                });
-            }
-
-            if (request.endsWith!==undefined) {
-                throw new Error('NOT_SUPPORTED');
-            }
-            if (request.contains!==undefined) {
-                throw new Error('NOT_SUPPORTED');
-            }
-
-            // apply the match criteria
-            if (filters.length>0) {
-                traverser.match(...filters).dedup();
-            }
-
-            // logger.debug(`search.full.dao buildSearchTraverser: traverser: ${JSON.stringify(traverser.toString())}`);
-
-            return traverser;
-        } finally {
-            conn.close();
+        let traverser: process.GraphTraversal;
+        if (request.ancestorPath!==undefined) {
+            const ancestorId = `group___${request.ancestorPath}`;
+            traverser = conn.traversal.V(ancestorId).
+                repeat(__.in_()).emit().as('a');
+        } else {
+            traverser = conn.traversal.V().as('a');
         }
+
+        // if authz is enabled, only return results that the user is authorized to view
+        if (authorizedPaths!==undefined && authorizedPaths.length>0) {
+            const authorizedPathIds = authorizedPaths.map(p=> `group___${p}`);
+            filters.push(__.as('a').until(__.hasId(...authorizedPathIds)).repeat(__.out()));
+        }
+
+        // construct all the filters that we will eventually pass to match()
+        if (request.types!==undefined) {
+            request.types.forEach(t=> filters.push(
+                __.as('a').hasLabel(t)) );
+        }
+
+        if (request.eq!==undefined) {
+            request.eq.forEach(f=> {
+                const filter = this.buildSearchFilterBase(f);
+                filters.push(filter.has(f.field, f.value));
+            });
+        }
+        if (request.neq!==undefined) {
+            request.neq.forEach(f=> {
+                const filter = this.buildSearchFilterBase(f);
+                filters.push(filter.not(__.has(f.field, f.value)));
+            });
+        }
+        if (request.lt!==undefined) {
+            request.lt.forEach(f=> {
+                const filter = this.buildSearchFilterBase(f);
+                filters.push(filter.values(f.field).is(process.P.lt(Number(f.value))));
+            });
+        }
+        if (request.lte!==undefined) {
+            request.lte.forEach(f=> {
+                const filter = this.buildSearchFilterBase(f);
+                filters.push(filter.values(f.field).is(process.P.lte(Number(f.value))));
+            });
+        }
+        if (request.gt!==undefined) {
+            request.gt.forEach(f=> {
+                const filter = this.buildSearchFilterBase(f);
+                filters.push(filter.values(f.field).is(process.P.gt(Number(f.value))));
+            });
+        }
+        if (request.gte!==undefined) {
+            request.gte.forEach(f=> {
+                const filter = this.buildSearchFilterBase(f);
+                filters.push(filter.values(f.field).is(process.P.gte(Number(f.value))));
+            });
+        }
+        if (request.startsWith!==undefined) {
+            request.startsWith.forEach(f=> {
+                const startValue = <string> f.value;
+                const nextCharCode = String.fromCharCode( startValue.charCodeAt(startValue.length-1) + 1);
+                const endValue = this.setCharAt(startValue, startValue.length-1, nextCharCode);
+
+                const filter = this.buildSearchFilterBase(f);
+                filters.push(filter.has(f.field, process.P.between(startValue, endValue)));
+            });
+        }
+
+        if (request.endsWith!==undefined) {
+            throw new Error('NOT_SUPPORTED');
+        }
+        if (request.contains!==undefined) {
+            throw new Error('NOT_SUPPORTED');
+        }
+
+        // apply the match criteria
+        if (filters.length>0) {
+            traverser.match(...filters).dedup();
+        }
+
+        // logger.debug(`search.full.dao buildSearchTraverser: traverser: ${JSON.stringify(traverser.toString())}`);
+
+        return traverser;
+
     }
 
     private buildSearchFilterBase(filter:SearchRequestFilter|SearchRequestFacet) : process.GraphTraversal {
@@ -139,18 +135,24 @@ export class SearchDaoFull extends BaseDaoFull {
     public async search(request: SearchRequestModel, authorizedPaths:string[], offset:number, count:number): Promise<Node[]> {
         logger.debug(`search.full.dao search: in: request: ${JSON.stringify(request)}, authorizedPaths:${authorizedPaths}, offset:${offset}, count:${count}`);
 
-        const traverser = this.buildSearchTraverser(request, authorizedPaths, offset, count);
+        let results;
+        const conn = super.getConnection();
+        try {
+            const traverser = this.buildSearchTraverser(conn, request, authorizedPaths, offset, count);
 
-        // apply pagination
-        if (offset!==undefined && count!==undefined) {
-            // note: workaround for weird typescript issue. even though offset/count are declared as numbers
-            // througout, they are being interpreted as strings within gremlin, therefore need to force to int beforehand
-            const offsetAsInt = parseInt(offset.toString(),0);
-            const countAsInt = parseInt(count.toString(),0);
-            traverser.range(offsetAsInt, offsetAsInt + countAsInt);
+            // apply pagination
+            if (offset!==undefined && count!==undefined) {
+                // note: workaround for weird typescript issue. even though offset/count are declared as numbers
+                // througout, they are being interpreted as strings within gremlin, therefore need to force to int beforehand
+                const offsetAsInt = parseInt(offset.toString(),0);
+                const countAsInt = parseInt(count.toString(),0);
+                traverser.range(offsetAsInt, offsetAsInt + countAsInt);
+            }
+            results = await traverser.select('a').valueMap().with_(process.withOptions.tokens).toList();
+        } finally {
+            conn.close();
         }
 
-        const results = await traverser.select('a').valueMap().with_(process.withOptions.tokens).toList();
         logger.debug(`search.full.dao search: results:${JSON.stringify(results)}`);
 
         if (results.length===0) {
@@ -171,24 +173,30 @@ export class SearchDaoFull extends BaseDaoFull {
     public async facet(request: SearchRequestModel, authorizedPaths:string[]): Promise<FacetResults> {
         logger.debug(`search.full.dao facet: in: request: ${JSON.stringify(request)}, authorizedPaths:${authorizedPaths}`);
 
-        const traverser = this.buildSearchTraverser(request, authorizedPaths);
+        let results;
+        const conn = super.getConnection();
+        try {
+            const traverser = this.buildSearchTraverser(conn, request, authorizedPaths);
 
-        if (request.facetField!==undefined) {
-            traverser.select('a');
-            if (request.facetField.traversals!==undefined) {
-                request.facetField.traversals.forEach(t=> {
-                    if (t.direction===SearchRequestFilterDirection.in) {
-                        traverser.in_(t.relation);
-                    } else {
-                        traverser.out(t.relation);
-                    }
-                });
+            if (request.facetField!==undefined) {
+                traverser.select('a');
+                if (request.facetField.traversals!==undefined) {
+                    request.facetField.traversals.forEach(t=> {
+                        if (t.direction===SearchRequestFilterDirection.in) {
+                            traverser.in_(t.relation);
+                        } else {
+                            traverser.out(t.relation);
+                        }
+                    });
+                }
+                traverser.values(request.facetField.field).groupCount();
             }
-            traverser.values(request.facetField.field).groupCount();
+            logger.debug(`search.full.dao buildSearchTraverser: traverser: ${JSON.stringify(traverser.toString())}`);
+            results = await traverser.next();
+        } finally {
+            conn.close();
         }
 
-        logger.debug(`search.full.dao buildSearchTraverser: traverser: ${JSON.stringify(traverser.toString())}`);
-        const results = await traverser.next();
         logger.debug(`search.full.dao facet: results: ${JSON.stringify(results)}`);
 
         if (results===undefined || results.value===undefined) {
@@ -205,9 +213,15 @@ export class SearchDaoFull extends BaseDaoFull {
     public async summary(request: SearchRequestModel, authorizedPaths:string[]): Promise<number> {
         logger.debug(`search.full.dao summarize: in: request: ${JSON.stringify(request)}, authorizedPaths:${authorizedPaths}`);
 
-        const traverser = this.buildSearchTraverser(request, authorizedPaths);
+        const conn = super.getConnection();
+        let result;
+        try {
+            const traverser = this.buildSearchTraverser(conn, request, authorizedPaths);
+            result = await traverser.select('a').count().next();
+        } finally {
+            conn.close();
+        }
 
-        const result = await traverser.select('a').count().next();
         const total = result.value as number;
         logger.debug(`search.full.dao summarize: exit: total: ${total}`);
         return total;
