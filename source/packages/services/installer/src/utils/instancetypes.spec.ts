7c26b26f2a46bf365b98af723ec9ad6dfb7b71d6
Jonas Neubert <jonasneu@amazon.com>
2022-03-15 18:29:11 +0000

    Squashed commit of the following
    commit ed795fd4 
    Author: Jonas Neubert <jonasneu@amazon.com> 
    Date: Thu Mar 10 2022 17:21:34 GMT-0800 (Pacific Standard Time) 
    
        changelog
    
    
    commit 8a522680 
    Author: Jonas Neubert <jonasneu@amazon.com> 
    Date: Thu Mar 10 2022 17:15:08 GMT-0800 (Pacific Standard Time) 
    
        chore(installer): remove unused inversify dependency
    
    
    commit 7715b2df 
    Author: Jonas Neubert <jonasneu@amazon.com> 
    Date: Thu Mar 10 2022 17:14:41 GMT-0800 (Pacific Standard Time) 
    
        fix(installer): fall back to input field if list of neptune instance types cannot be loaded
    
    
    commit 7008019a 
    Author: Jonas Neubert <jonasneu@amazon.com> 
    Date: Wed Mar 09 2022 23:58:15 GMT-0800 (Pacific Standard Time) 
    
        feat(installers): List of available Neptune instance types fetched from RDS API
    
    
    commit 39f08d09 
    Author: Jonas Neubert <jonasneu@amazon.com> 
    Date: Wed Mar 09 2022 14:41:52 GMT-0800 (Pacific Standard Time) 
    
        fix(assetlibrary): Move default values for Neptune instance type from CFN to bash/installer
    
    
    commit 8439deb6 
    Author: Jonas Neubert <jonasneu@amazon.com> 
    Date: Wed Mar 09 2022 22:47:59 GMT-0800 (Pacific Standard Time) 
    
        fix(assetlibrary): expand list of accepted Neptune instance types

---
+++ source/packages/services/installer/src/utils/instancetypes.spec.ts
@@ -0,0 +1,74 @@
+/*********************************************************************************************************************
+ *  Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.                                           *
+ *                                                                                                                    *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance    *
+ *  with the License. A copy of the License is located at                                                             *
+ *                                                                                                                    *
+ *      http://www.apache.org/licenses/LICENSE-2.0                                                                    *
+ *                                                                                                                    *
+ *  or in the'license' file accompanying this file. This file is distributed on an'AS IS' BASIS, WITHOUT WARRANTIES *
+ *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions    *
+ *  and limitations under the License.                                                                                *
+ *********************************************************************************************************************/
+import { compareNeptuneInstancetypeNames } from './instancetypes';
+
+describe('compareNeptuneInstancetypes', () => {
+  it('should sort instance types by instanceFamily and tshirt size', async () => {
+    const sorted = [
+      'db.r5.large',
+      'db.r5.xlarge',
+      'db.r5.2xlarge',
+      'db.r5.4xlarge',
+      'db.r5.8xlarge',
+      'db.r5.12xlarge',
+      'db.r5.16xlarge',
+      'db.r5.24xlarge',
+      'db.r5d.large',
+      'db.r5d.xlarge',
+      'db.r5d.2xlarge',
+      'db.r5d.4xlarge',
+      'db.r5d.8xlarge',
+      'db.r5d.12xlarge',
+      'db.r5d.24xlarge',
+      'db.r6g.large',
+      'db.r6g.xlarge',
+      'db.r6g.2xlarge',
+      'db.r6g.4xlarge',
+      'db.r6g.8xlarge',
+      'db.r6g.12xlarge',
+      'db.r6g.16xlarge',
+      'db.t3.medium',
+      'db.t4g.medium',
+    ];
+
+    const shuffled = [
+      'db.r6g.8xlarge',
+      'db.r5d.12xlarge',
+      'db.r5.large',
+      'db.r5d.xlarge',
+      'db.r6g.12xlarge',
+      'db.r5d.8xlarge',
+      'db.r6g.large',
+      'db.r5d.4xlarge',
+      'db.r6g.4xlarge',
+      'db.r5d.2xlarge',
+      'db.r6g.16xlarge',
+      'db.r5.16xlarge',
+      'db.r5d.large',
+      'db.t4g.medium',
+      'db.r6g.xlarge',
+      'db.t3.medium',
+      'db.r5.4xlarge',
+      'db.r5.8xlarge',
+      'db.r5.xlarge',
+      'db.r5.2xlarge',
+      'db.r5d.24xlarge',
+      'db.r5.12xlarge',
+      'db.r5.24xlarge',
+      'db.r6g.2xlarge',
+    ];
+
+    shuffled.sort(compareNeptuneInstancetypeNames);
+    expect(shuffled).toEqual(sorted);
+  });
+});
