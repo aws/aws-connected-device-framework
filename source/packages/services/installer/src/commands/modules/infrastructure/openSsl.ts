d16701c5e9c46bab9af047f766ed5f13478cdbc9
willsia <willsia@amazon.com>
2022-04-27 14:31:14 +0800

    ensure the text is consistent

--- source/packages/services/installer/src/commands/modules/infrastructure/openSsl.ts
+++ source/packages/services/installer/src/commands/modules/infrastructure/openSsl.ts
@@ -10,7 +10,6 @@
  *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions    *
  *  and limitations under the License.                                                                                *
  *********************************************************************************************************************/
-import execa from 'execa';
 import inquirer from 'inquirer';
 import { ListrTask } from 'listr2';
 import ow from 'ow';
@@ -83,7 +82,7 @@ export class OpenSslInstaller implements InfrastructureModule {
 
     if (answers.openSsl.deploy) {
       tasks.push({
-        title: `Packaging stack '${this.stackName}'`,
+        title: `Packaging and deploying stack '${this.stackName}'`,
         task: async () => {
           await packageAndDeployStack({
             answers: answers,
